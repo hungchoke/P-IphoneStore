d8ba3ab5c4154dbfafc4baa89a1cca9f395b0071
hungchoke <cloneleviathan@gmail.com>
2021-09-05 16:18:08 +0700

    .

--- SQLDB/LoginDB.sql
+++ SQLDB/LoginDB.sql
@@ -17,5 +17,8 @@ grant all on LoginDB.* to 'iphonestaff'@'localhost';
 
 insert into Staffs(staff_name, user_name, user_pass, role) values
 	('Sale','sale','a288c42085685d673b9450d539e06695',1);
+insert into Staffs(staff_name, user_name, user_pass, role) values
+	('Accountant','accountant','dcb7cfc8aa5d2c0774f4ddb1abba7362',2);
 select * from Staffs;
-select * from Staffs where user_name= 'sale' and user_pass ='085685d673b9450d539e06695';
+select * from Staffs where user_name= 'sale' and user_pass ='a288c42085685d673b9450d539e06695';
+select * from Staffs where user_name= 'accountant' and user_pass ='dcb7cfc8aa5d2c0774f4ddb1abba7362';
